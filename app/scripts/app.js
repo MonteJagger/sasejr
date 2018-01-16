608f5922ef638343f69c834b4e6d09d4c705642a
Hiumathy <hiumathy@Hiumathys-MacBook-Pro.local>
2018-01-15 18:44:06 -0600

    yo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sasejrApp
+ * @description
+ * # sasejrApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sasejrApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sasejrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sasejrApp
+ */
+angular.module('sasejrApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
